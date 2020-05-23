ecc555d91524926a5e0fbc6e6aadb3e32df388b8
siddharthtank <62084107+siddharthtank@users.noreply.github.com>
2020-05-23 18:27:45 +0530

    Add files via upload

--- js/load-player.js
+++ js/load-player.js
@@ -1,10 +1,1 @@
-function myFunction(x) {
-    if (x.matches) { 
-        revealPlayer();
-    }
-}
-
-var x = window.matchMedia("(max-width: 992px)")
-    myFunction(x)
-    x.addListener(myFunction)
-
+function myFunction(n){n.matches&&revealPlayer()}var x=window.matchMedia("(max-width: 992px)");myFunction(x),x.addListener(myFunction);
