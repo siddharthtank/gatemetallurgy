83a7c48bba25ee77f1e8325b0076698073a36e39
siddharthtank <62084107+siddharthtank@users.noreply.github.com>
2020-05-31 07:14:54 +0530

    Add files via upload

---
+++ assets/php/footer.php
@@ -0,0 +1,38 @@
+<section class="footer">
+    <div class="container">
+        <div>
+          <p>GATE MT Info</p>
+          <ul>
+              <li><a href="#">Overview</a></li>
+              <li><a href="#">Eligibility Criteria</a></li>
+              <li><a href="#">Exam Syllabus</a></li>
+              <li><a href="#">FAQs</a></li>
+          </ul>
+        </div>
+        <div>
+          <p>Video Courses</p>
+          <ul>
+              <li><a href="#">Physical Metallurgy</a></li>
+              <li><a href="#">Mechanical Metallurgy</a></li>
+              <li><a href="#">Thermodynamics and Rate Processes</a></li>
+              <li><a href="#">Extractive Metallurgy</a></li>
+              <li><a href="#">Manufacturing Processes</a></li>
+          </ul>
+        </div>
+        <div>
+          <p>Contact Us</p>
+          <ul>
+              <li><a href="https://www.instagram.com/gate_metallurgy" target="_blank"><img src="../../../icons/insta.png">@gate_metallurgy</a></li>
+              <li><a href="mailto: info.gatemetallurgy@gmail.com"><img src="../../../icons/email.png">info.gatemetallurgy@gmail.com</a></li>
+              <a href="#"><img data-src="../../../icons/gplay.svg" id="gplay"></a>
+          </ul>
+        </div>
+    </div>
+</section>
+  
+<footer>
+    <div class="container">
+        <P>&copy; 2020 GATE Metallurgy. All rights reserved.</P>
+        <a href="#"><img src="../../../icons/logo-light.svg" alt="" id="footer-logo"></a>
+    </div>
+</footer>
---
+++ assets/php/header.php
@@ -0,0 +1,39 @@
+<header>
+    <div class="container">
+        <a href="#"><img data-src="../../../icons/logo-light.svg" alt="" class="logo" id="header-logo"></a>
+        <div class="navbar">
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">GATE Info</a></li>
+                    <li><a href="#">Video Courses</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </nav>
+            <div class="mode">
+                <p>Dark</p>
+                <label class="switch">
+                    <input type="checkbox" name="theme">
+                     <span class="slider round"></span>
+                </label>
+            </div>
+            <div class="nav">
+              <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu">
+              <label for="openSidebarMenu" class="sidebarIconToggle">
+                <div class="spinner diagonal part-1"></div>
+                <div class="spinner horizontal"></div>
+                <div class="spinner diagonal part-2"></div>
+              </label>
+
+              <div id="sidebarMenu">
+                <ul class="sidebarMenuInner">
+                    <a href="https://gatemetallurgy.tk"><li>Home</li></a>
+                    <a href="#"><li>GATE Info</li></a>
+                    <a href="#"><li>Video Course</li></a>
+                    <a href="#"><li>Contact Us</li></a>
+                </ul>
+              </div>
+          </div>
+        </div>
+    </div>
+</header>
